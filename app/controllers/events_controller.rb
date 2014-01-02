9c4af94e38bce3ba4c3f7a7a4fa9671029d85046
Shane Moore <shanewmoore@gmail.com>
2014-01-02 15:17:20 -0500

    Fix problem with rendering event index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   # before_action :exec, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @events = Event.all
+    @events = Event.paginate(page: params[:page])
   end
 
   def new
